e595c68f138f4fa920b96b0eb34de07b74663384
francescozollo1998 <62211236+francescozollo1998@users.noreply.github.com>
2022-11-11 14:30:32 +0100

    Aggiunta "esercizio2_scanner_if"
    11/11/2022 (Francesco Zollo)

---
+++ esercizio2_scanner_if.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;       // importo la classe Scanner della libreria util
+
+class principale {
+    public static void main(String[] argv){
+        Scanner scan = new Scanner(System.in);      // scan e' una istanza di Scanner
+
+        // richiesta del primo numero da tastiera
+        System.out.print("Inserire il primo numero: ");
+        int primo = scan.nextInt();
+        
+        // richiesta del secondo numero da tastiera
+        System.out.print("Inserire il secondo numero: ");
+        int secondo = scan.nextInt();
+
+        // stampa del risultato
+        System.out.print("Il minore e': ");
+        
+        if(primo > secondo){        // se secondo e' il minore lo stampo
+            System.out.print(secondo);
+        }       // fine if
+
+        if(secondo > primo){        // se primo e' il minore lo stampo
+            System.out.print(primo);
+        }       // fine if
+
+        if(primo == secondo){       // se sono uguali non stampo nessuno
+            System.out.print("nessuno, sono uguali.");
+        }       // fine if
+    }       // fine main
+}       // fine classe
